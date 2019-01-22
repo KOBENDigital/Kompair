57de504deda4047d087fb94d1d2e82b20d26212e
Samuel Butler <samuelbutler87@gmail.com>
2019-01-22 16:35:07 +1000

    started work on adding auth mode

--- Koben.Kompair/KompairDefaults.cs
+++ Koben.Kompair/KompairDefaults.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Cryptography.X509Certificates;
+using Koben.Kompair;
 
 namespace Koben.SanityCheck
 {
@@ -14,11 +10,11 @@ namespace Koben.SanityCheck
 		public static string CertificateStoreNameAppSetting = "Kompair.StoreName";
 		public static string CertificateStoreLocationAppSetting = "Kompair.StoreLocation";
 		public static string ValidCertificatesOnlyAppSetting = "Kompair.ValidCertificatesOnly";
-		public static string SkipAuthenticationAppSetting = "Kompair.SkipAuthentication";
+		public static string AuthenticationModeAppSetting = "Kompair.AuthenticationMode";
 
 		public static StoreName CertificateStoreName = StoreName.My;
 		public static StoreLocation CertificateStoreLocation = StoreLocation.CurrentUser;
 		public static bool ValidCertificatesOnly = true;
-		public static bool SkipAuthentication = false;
+		public static KompairAuthenticationMode AuthenticationMode = KompairAuthenticationMode.Certificate;
 	}
 }
